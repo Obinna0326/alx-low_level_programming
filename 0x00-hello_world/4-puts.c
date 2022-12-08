f3a64fd8fe9ecf0e89d3b91be4b711bb452bbf17
Obinna0326 <onyemaobinna24@gmail>
2022-12-08 08:59:38 -0800

    Put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <studio.h>
-
 /** 
  * main - Main entry prints hello,world
+ *
  * Return: Always 0 (success)
  */
 
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzl\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
