4ac9cfb76716f01c27ba75358fbcaa2e3bdb01b0
Obinna0326 <onyemaobinna24@gmail>
2022-12-08 20:36:21 -0800

    Unix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
-#include <unistd.h>                                                                                          
-/** 
- * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+#include <unistd.h>                                                                     /** 
+ * main - Prints "and that piece of art is useful"
  * Return: Always
 */
 int main(void)
