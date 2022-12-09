ec62d6e622c7fa0c1d74e8e172419b87543e5a8f
Obinna0326 <onyemaobinna24@gmail>
2022-12-08 20:32:01 -0800

    Unix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,11 @@
-#include <stdio>
-#include <unistd.h>
-
-/**
- * main - Main Function to write a quote
- *
- * Return: Return 1 if successful
- */
-
+#include <stdio.h>
+#include <unistd.h>                                                                                          
+/** 
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ * Return: Always
+*/
 int main(void)
 {
-	write(2,
-		"and that piece of art is useful" - Dora Korpar, 2015-10-19\n",
-		59);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
