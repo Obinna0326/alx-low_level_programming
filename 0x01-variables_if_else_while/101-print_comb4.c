d7e1f3b9da993308729d80f13a7f25b5b007feb8
Obinna0326 <onyemaobinna24@gmail>
2022-12-11 12:36:35 -0800

    helo

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+/**
+ * main - entry point of the program
+ *
+ * Return:Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+	int b;
+	int c;
+
+	for (a = 0; a < 8; a++)
+	{
+		for (b = 0; b < 9; b++)
+		{
+			for (c = 0; c < 10; c++)
+		{
+				if (a == b)
---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/**
+ * main - Entry point of program
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a < 100; a++)
+	{
+		for (b = 0; b < 100; b++)
+		{
+			if (a < b)
+			{
+			putchar((a / 10) + '0');
+			putchar((a % 10) + '0');
+			putchar(' ');
+			putchar((b / 10) + '0');
+			putchar((b % 10) + '0');
+
+			if (a != 98 || b != 99)
+		{
+
+			putchar(',');
+			putchar(' ');
+		}
+		}
+		}
+		}
+
+	putchar('\n');
+	return (0);
+}
