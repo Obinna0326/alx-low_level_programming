50879a476ca6da275d1ba0e9fe08650163713e57
Obinna0326 <onyemaobinna24@gmail.com>
2022-12-15 11:26:03 -0800

    The numbers speak for themselves

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -5,12 +5,12 @@
  */
 void print_numbers(void)
 {
-	int a = 0;
+		int a = 0;
 
-	while (a <= 9)
+		while (a <= 9)
 	{
-		_putchar(a + '0');
-		a++
+	_putchar(a + '0');
+	a++
 	}
 	_putchar('\n');
 }
