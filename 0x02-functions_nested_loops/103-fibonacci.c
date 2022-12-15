7c0f7ea03fd82ca7fc9de6117e8935244d610439
Obinna0326 <onyemaobinna24@gmail.com>
2022-12-15 11:16:37 -0800

    Collaboration is multiplication

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - finds and prints the sum of the even-valued terms
+ * followed by a new line
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	unsigned long int j, k, next, sum;
+
+	j = 1;
+	k = 2;
+	sum = 0;
+
+	for (i = 1; i <= 33; ++i)
+
