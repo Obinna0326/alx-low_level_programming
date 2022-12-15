0b838ac0dbc059eb8453206eb1034ecb4a9007b0
Obinna0326 <onyemaobinna24@gmail.com>
2022-12-15 08:50:02 -0800

    Collaboration is multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *main - Entry point of the program
+ *@a: carrier integer variable
+ *@b: carrier integer variable
+ *Return: 0
+ */
+int mul(int a, int b);
+{
+	int (int a * int b)
+	{
+		return (0);
+	}
+}
