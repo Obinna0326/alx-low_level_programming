569c21839143499cdd47a4d4fa5c458f83e23ef3
Obinna0326 <onyemaobinna24@gmail.com>
2022-12-15 07:28:24 -0800

    isdigit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,8 +1,8 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * _isupper - checks for uppercase character
- * @c - The character to be checked
+ * @c:carrier variable
  * Return:1 if c is uppercase, 0 otherwise
  */
 int _isupper(int c)
