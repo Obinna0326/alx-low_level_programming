d2027614cadeaa5e046f60ba723078bacac67b92
Obinna0326 <onyemaobinna24@gmail.com>
2022-12-15 07:08:43 -0800

    Uppercase

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,14 +3,12 @@
 /**
  * _isupper - checks for uppercase character
  * @c - The character to be checked
- * Return: Returns 1 if c is uppercase
- * and 0 otherwise
+ * Return:1 if c is uppercase, 0 otherwise
  */
 int _isupper(int c)
 {
-	char c;
 
-	if (c >= 'A')
+	if (c >= 'A' && c <= 'z')
 	{
 		return (1);
 	}
@@ -18,6 +16,4 @@ int _isupper(int c)
 	{
 		return (0);
 	}
-
-
 }
