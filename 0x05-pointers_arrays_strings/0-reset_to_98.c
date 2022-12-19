a1bfb9296358ee03c674a20162b869471d85c2aa
Obinna0326 <onyemaobinna24@gmail.com>
2022-12-19 09:03:26 -0800

    98 Battery st

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main -  function that takes a pointer to an int as parameter
+ * Return: Always 0 (sucess)
+ */
+
+void reset_to_98(int *n)
+{
+int *b n;
+n = 98;
+
+b = &n;
+
+printf("%d\n", n);
+return (0);
+}
+
