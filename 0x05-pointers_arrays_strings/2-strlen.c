f5b7717ab926a121fe229cd265750d9afec1de33
Obinna0326 <onyemaobinna24@gmail.com>
2022-12-20 00:32:01 -0800

    Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strlen - A function that returns the length of a string
+ * @s: Stings to be returned
+ * Return: Always 0 (sucess)
+ */
+
+int _strlen(char *s)
+{
+int i;
+i = 0;
+
+while (a[i])
+i++
+
+return (i);
+}
