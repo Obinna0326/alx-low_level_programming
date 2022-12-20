a162408391d8301d2d90f35a73f9ecd390137002
Obinna0326 <onyemaobinna24@gmail.com>
2022-12-20 05:05:55 -0800

    pointer, arrays, strings

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * _puts - prints a string to stdout
  * @str: pointer to the string to print
@@ -9,7 +10,6 @@ void _puts(char *str)
 	
 	for (i = 0; str[i] != '\0'; i++)
 {
-		_putchar(str[i]);
 }
 		_putchar('\n');
 }
