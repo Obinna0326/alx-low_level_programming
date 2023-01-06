d99f06213b512f1bd87851fcde1802982cfc4787
Obinna0326 <onyemaobinna24@gmail.com>
2023-01-05 20:34:43 -0800

    recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,10 +1,9 @@
-#include "holberton.h"
-/**
- *_print_rev_recursion -   prints a string in reverse
- *@s: pointer block of memory to fill
- *Return: void
- */
+#include "main.h"
 
+/**
+ * _print_rev_recursion - prints a string in reverse
+ * @s: string to print
+ */
 void _print_rev_recursion(char *s)
 {
 	if (*s)
